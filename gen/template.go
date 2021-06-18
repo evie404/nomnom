56bfb38dbd5f245ec94e16326648f34f403ae3df
Ricky Pai <rickyp999+github@gmail.com>
2021-06-17 18:11:52 -0700

    runTemplate

--- gen/template.go
+++ gen/template.go
@@ -9,28 +9,15 @@ import (
 )
 
 func ValuesStructTemplate(enum Enum) ([]byte, error) {
-	rawTemplate, err := ioutil.ReadFile(filepath.Join("templates", "values_struct.go.tmpl"))
-	if err != nil {
-		return nil, fmt.Errorf("reading template file: %w", err)
-	}
-
-	t, err := template.New("letter").Parse(string(rawTemplate))
-	if err != nil {
-		return nil, fmt.Errorf("parsing template: %w", err)
-	}
-
-	var b bytes.Buffer
-
-	err = t.Execute(&b, enum)
-	if err != nil {
-		return nil, fmt.Errorf("executing template: %w", err)
-	}
-
-	return b.Bytes(), nil
+	return runTemplate(filepath.Join("templates", "values_struct.go.tmpl"), enum)
 }
 
 func ConversionsTemplate(enum Enum) ([]byte, error) {
-	rawTemplate, err := ioutil.ReadFile(filepath.Join("templates", "conversions.go.tmpl"))
+	return runTemplate(filepath.Join("templates", "conversions.go.tmpl"), enum)
+}
+
+func runTemplate(templatePath string, enum Enum) ([]byte, error) {
+	rawTemplate, err := ioutil.ReadFile(templatePath)
 	if err != nil {
 		return nil, fmt.Errorf("reading template file: %w", err)
 	}
